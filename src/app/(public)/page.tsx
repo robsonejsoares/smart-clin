b406026a7fcaad6f866b8be62b196c2b52879ebd
Robson Edvaldo José Soares <robsoncsoares.1050@gmail.com>
2026-09-09 21:36:15 -0300

    fix: ajusta layout, alterando favicon e a logo

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,17 +1,17 @@
 import { Footer } from "./_components/footer";
 import { Header } from "./_components/header";
 import { Hero } from "./_components/hero";
-import { Professionals } from "./_components/professionals"
+import { Professionals } from "./_components/professionals";
 
 export default function Home() {
-  return (
-    <div className="flex flex-col min-h-screen">
-      <Header />
-      <div>
-        <Hero />
-        <Professionals />
-        <Footer />
-      </div>
-    </div>
-  );
+    return (
+        <div className="flex flex-col min-h-screen">
+            <Header />
+            <main className="flex-1">
+                <Hero />
+                <Professionals />
+            </main>
+            <Footer />
+        </div>
+    );
 }
