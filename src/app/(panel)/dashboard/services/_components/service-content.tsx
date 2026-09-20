7731a7d2a069a5f388c8eb31663858166e5232b8
Robson Edvaldo José Soares <robsoncsoares.1050@gmail.com>
2026-09-20 00:35:49 -0300

    feat(service): altera service contento tsx

--- src/app/(panel)/dashboard/services/_components/service-content.tsx
+++ src/app/(panel)/dashboard/services/_components/service-content.tsx
@@ -9,7 +9,7 @@ export async function ServicesContent({ userId }: ServicesContentProps) {
 
     const services = await getAllServices({ userId: userId })
 
-    console.log("teste-historico-linear-pr", services)
+    console.log("nós vamos apagar isso aqui", services)
 
     return (
         <ServicesList />
