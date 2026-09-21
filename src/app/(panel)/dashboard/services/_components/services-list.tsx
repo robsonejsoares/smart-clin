633c6742bcaae8e6be1fccaeef54e551b549128d
Robson Edvaldo José Soares <robsoncsoares.1050@gmail.com>
2026-09-21 11:48:10 -0300

    feat(service): Cadastra serviço da clínica

--- src/app/(panel)/dashboard/services/_components/services-list.tsx
+++ src/app/(panel)/dashboard/services/_components/services-list.tsx
@@ -1,23 +1,37 @@
 "use client"
 
 import {
+    X,
+    Plus,
+    Pencil,
+} from "lucide-react";
+
+import {
     Card,
-    CardHeader,
     CardTitle,
+    CardHeader,
+    CardContent,
 } from "@/components/ui/card";
 
 import {
     Dialog,
-    DialogTrigger,
     DialogContent,
+    DialogTrigger,
 } from "@/components/ui/dialog";
 
 import { useState } from "react";
-import { Plus } from "lucide-react";
-import { DialogService } from "./dialog-service";
 import { Button } from "@/components/ui/button";
+import { DialogService } from "./dialog-service";
+import { Service } from "@/generated/prisma/client";
+import { formatCurrency } from "@/utils/formatCurrency";
 
-export function ServicesList() {
+interface ServicesListProps {
+    services: Service[];
+}
+
+export function ServicesList({ services }: ServicesListProps) {
+
+    console.log("services", services)
 
     const [isDialog, setIsDialogOpen] = useState(false);
 
@@ -35,9 +49,44 @@ export function ServicesList() {
                             </Button>
                         </DialogTrigger>
                         <DialogContent>
-                            <DialogService />
+                            <DialogService
+                                closeModal={() => {
+                                    setIsDialogOpen(false)
+                                }}
+                            />
                         </DialogContent>
                     </CardHeader>
+                    <CardContent>
+                        <section className="space-y-4 mt-5">
+                            {services.map(service => (
+                                <article
+                                key={service.id}
+                               className="flex items-center justify-between">
+                                    <div className="flex items-center space-x-2">
+                                        <span className="font-medium">{service.name}</span>
+                                        <span className="text-gray-500">-</span>
+                                        <span className="text-gray-500">{formatCurrency((service.price / 100))}</span>
+                                    </div>
+                                    <div>
+                                        <Button
+                                            variant="ghost"
+                                            size="icon"
+                                            onClick={() => { }}
+                                        >
+                                            <Pencil className="h-4 w-4" />
+                                        </Button>
+                                        <Button
+                                            variant="ghost"
+                                            size="icon"
+                                            onClick={() => { }}
+                                        >
+                                            <X className="h-4 w-4" />
+                                        </Button>
+                                    </div>
+                                </article>
+                            ))}
+                        </section>
+                    </CardContent>
                 </Card>
             </section>
         </Dialog>
