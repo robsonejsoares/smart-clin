99a6a2c344a8418468de124ef4b9eb72f9072bc7
Robson Edvaldo José Soares <robsoncsoares.1050@gmail.com>
2026-09-25 23:35:14 -0300

    feat(dashboard) Começando dashboard agendamentos

---
+++ src/app/(panel)/dashboard/components/button-copy-link.tsx
@@ -0,0 +1,39 @@
+"use client"
+
+import { toast } from "sonner"
+import { LinkIcon } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipTrigger,
+} from "@/components/ui/tooltip"
+
+export function ButtonCopyLink({ userId }: { userId: string }) {
+
+    async function handleCopyLink() {
+        await navigator.clipboard.writeText(
+            `${process.env.NEXT_PUBLIC_URL}/clinica/${userId}`
+        )
+
+        toast.success("Link copiado com sucesso!")
+    }
+
+    return (
+        <Tooltip>
+            <TooltipTrigger asChild>
+                <Button
+                    variant="outline"
+                    size="icon"
+                    onClick={handleCopyLink}
+                >
+                    <LinkIcon className="h-4 w-4" />
+                </Button>
+            </TooltipTrigger>
+
+            <TooltipContent>
+                Copiar link
+            </TooltipContent>
+        </Tooltip>
+    )
+}
--- src/app/(panel)/dashboard/page.tsx
+++ src/app/(panel)/dashboard/page.tsx
@@ -1,5 +1,10 @@
+import Link from "next/link";
+import { Calendar } from "lucide-react";
 import getSession from "@/lib/getSession";
 import { redirect } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { ButtonCopyLink } from "./components/button-copy-link";
 
 export default async function Dashboard() {
     const session = await getSession();
@@ -9,13 +14,22 @@ export default async function Dashboard() {
     }
 
     return (
-        <div>
-            <h1>
-                Página Dashboard
-            </h1>
-            <div className="w-full h-150 bg-gray-200 mb-10"></div>
-            <div className="w-full h-150 bg-gray-500 mb-10"></div>
-            <div className="w-full h-150 bg-gray-300 mb-10"></div>
-        </div>
+        <main>
+            <div className="flex items-center justify-end space-x-2">
+                <TooltipProvider>
+                    <ButtonCopyLink userId={session.user?.id as string} />
+
+                    <Link
+                        href={`/clinica/${session.user?.id}`}
+                        target="_blank"
+                    >
+                        <Button className="bg-emerald-500 hover:bg-emerald-400 font-semibold">
+                            <Calendar className="w-5 h-5" />
+                            <span>Novo Agendamento</span>
+                        </Button>
+                    </Link>
+                </TooltipProvider>
+            </div>
+        </main>
     );
 }
